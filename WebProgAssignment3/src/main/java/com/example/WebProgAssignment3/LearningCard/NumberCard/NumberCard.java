4446f0f8270475ab4cbc334f1db20fad5905d645
krissi15 <115079713+krissi15@users.noreply.github.com>
2022-10-15 13:54:14 +0200

    Delete WebProgAssignment3 directory

--- WebProgAssignment3/src/main/java/com/example/WebProgAssignment3/LearningCard/NumberCard/NumberCard.java
+++
@@ -1,7 +0,0 @@
-package com.example.WebProgAssignment3.LearningCard.NumberCard;
-
-import com.example.WebProgAssignment3.LearningCard.LearningCard;
-
-public class NumberCard extends LearningCard {
-
-}
