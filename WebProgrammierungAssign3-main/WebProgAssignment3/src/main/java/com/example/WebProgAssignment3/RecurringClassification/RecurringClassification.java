e7e7d1f05bb9ffeb3ab8e4623a63a9558aa3aa7d
krissi15 <115079713+krissi15@users.noreply.github.com>
2022-10-20 17:24:02 +0200

    Delete WebProgrammierungAssign3-main directory

--- WebProgrammierungAssign3-main/WebProgAssignment3/src/main/java/com/example/WebProgAssignment3/RecurringClassification/RecurringClassification.java
+++
@@ -1,9 +0,0 @@
-package com.example.WebProgAssignment3.RecurringClassification;
-
-public class RecurringClassification {
-    private Long timeInMilliseconds;
-
-    public Long getTimeInMilliseconds() {
-        return timeInMilliseconds;
-    }
-}
